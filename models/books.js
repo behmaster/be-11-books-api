290353d827535695f19a23ac9ea156d1240aadcb
Robbie Behm <behmaster@gmail.com>
2022-06-30 21:14:13 -0500

    API HW

---
+++ models/books.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+// schema:
+const bookSchema = new Schema({
+  title: { type: String, required: true },
+  description: String,
+  year: Number,
+  quantity: Number,
+  imageURL: String,
+});
+
+//MODEL AND EXPORT
+const Book = mongoose.model("Book", bookSchema);
+module.exports = Book;
---
+++ server.js
@@ -0,0 +1,31 @@
+// DEPENDENCIES
+const express = require("express");
+const mongoose = require("mongoose");
+
+// CONFIGURATION
+require("dotenv").config();
+const PORT = process.env.PORT;
+const app = express();
+mongoose.connect(
+  process.env.MONGO_URI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("connected to mongo: ", process.env.MONGO_URI);
+  }
+);
+
+// MIDDLEWARE
+app.use(express.urlencoded({ extended: true }));
+
+const booksController = require("./controllers/books-controller.js");
+app.use("/books", booksController);
+
+//ROUTES
+app.get("/", (req, res) => {
+  res.send("Hello world!");
+});
+
+// LISTEN
+app.listen(PORT, () => {
+  console.log("Greetings! From port: ", PORT);
+});
